4988af730b33a4bbda12113fac03d91f840fda32
Sergey Bespalov <sergey.bespalov@chisw.us>
2013-08-20 15:28:51 +0300

    implement new functionality

--- server.php
+++ server.php
@@ -1,164 +1,183 @@
 <?php
 
-die(1);
+/* 	>php -q server.php	 */
+
 error_reporting(E_ALL);
 set_time_limit(0);
 ob_implicit_flush();
 
-$master  = WebSocket("localhost",123456);
-$sockets = array($master);
-$users   = array();
-$debug   = false;
+$address = "127.0.0.1";
+$port = "1222";
+GLOBAL $clients;
+GLOBAL $client_list;
 
-while(true){
-  $changed = $sockets;
-  socket_select($changed,$write=NULL,$except=NULL,NULL);
-  foreach($changed as $socket){
-    if($socket==$master){
-      $client=socket_accept($master);
-      if($client<0){ console("socket_accept() failed"); continue; }
-      else{ connect($client); }
+// socket creation
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+
+if (!is_resource($socket))
+    console("socket_create() failed: " . socket_strerror(socket_last_error()), true);
+
+if (!socket_bind($socket, $address, $port))
+    console("socket_bind() failed: " . socket_strerror(socket_last_error()), true);
+
+if (!socket_listen($socket, 20))
+    console("socket_listen() failed: " . socket_strerror(socket_last_error()), true);
+
+console("Server started on $address : $port\n\n");
+
+$master = $socket;
+$sockets = array($socket);
+
+while (true) {
+    $changed = $sockets;
+    foreach ($changed as $socket) {
+
+        if ($socket == $master) {
+
+            // new client will enter in this case and connect with server
+
+            socket_select($changed, $write = NULL, $except = NULL, NULL);
+            console("Master Socket Changed.\n\n");
+            $client = socket_accept($master);
+            if ($client < 0) {
+                console("socket_accept() failed\n\n");
+                continue;
+            } else {
+                console("Connecting socket.\n\n");
+                fnConnectacceptedSocket($socket, $client);
+                $master = null;
+            }
+        } else {
+
+            // clients who are connected with server will enter into this case
+            // first client will handshake with server and then exchange data with server
+
+            $client = getClientBySocket($socket);
+            if ($client) {
+
+                if ($clients[$socket]["handshake"] == false) {
+                    $bytes = @socket_recv($client, $data, 2048, MSG_DONTWAIT);
+                    if ((int) $bytes == 0)
+                        continue;
+
+                    console("Handshaking headers from client:" . $data);
+                    if (handshake($client, $data, $socket))
+                        $clients[$socket]["handshake"] = true;
+                } else if ($clients[$socket]["handshake"] == true) {
+                    $bytes = @socket_recv($client, $data, 2048, MSG_DONTWAIT);
+                    if ($data != "") {
+                        $decoded_data = unmask($data);
+                        socket_write($client, encode("You have entered: " . $decoded_data));
+                        console("Data from client:" . $decoded_data);
+                        socket_close($socket);
+                    }
+                }
+            }
+        }
     }
-    else{
-      $bytes = @socket_recv($socket,$buffer,2048,0);
-      if($bytes==0){ disconnect($socket); }
-      else{
-        $user = getuserbysocket($socket);
-        if(!$user->handshake){ dohandshake($user,$buffer); }
-        else{ process($user,$buffer); }
-      }
+}
+
+# Close the master sockets
+socket_close($socket);
+
+function unmask($payload) {
+    $length = ord($payload[1]) & 127;
+
+    if ($length == 126) {
+        $masks = substr($payload, 4, 4);
+        $data = substr($payload, 8);
+    } elseif ($length == 127) {
+        $masks = substr($payload, 10, 4);
+        $data = substr($payload, 14);
+    } else {
+        $masks = substr($payload, 2, 4);
+        $data = substr($payload, 6);
     }
-  }
+
+    $text = '';
+    for ($i = 0; $i < strlen($data); ++$i) {
+        $text .= $data[$i] ^ $masks[$i % 4];
+    }
+    return $text;
 }
 
-//---------------------------------------------------------------
-function process($user,$msg){
-  $action = unwrap($msg);
-  say("< ".$action);
-  switch($action){
-    case "hello" : send($user->socket,"hello human");                       break;
-    case "hi"    : send($user->socket,"zup human");                         break;
-    case "name"  : send($user->socket,"my name is Multivac, silly I know"); break;
-    case "age"   : send($user->socket,"I am older than time itself");       break;
-    case "date"  : send($user->socket,"today is ".date("Y.m.d"));           break;
-    case "time"  : send($user->socket,"server time is ".date("H:i:s"));     break;
-    case "thanks": send($user->socket,"you're welcome");                    break;
-    case "bye"   : send($user->socket,"bye");                               break;
-    default      : send($user->socket,$action." not understood");           break;
-  }
+function encode($text) {
+    // 0x1 text frame (FIN + opcode)
+    $b1 = 0x80 | (0x1 & 0x0f);
+    $length = strlen($text);
+
+    if ($length <= 125)
+        $header = pack('CC', $b1, $length);
+    elseif ($length > 125 && $length < 65536)
+        $header = pack('CCS', $b1, 126, $length);
+    elseif ($length >= 65536)
+        $header = pack('CCN', $b1, 127, $length);
+
+    return $header . $text;
 }
 
-function send($client,$msg){
-  say("> ".$msg);
-  $msg = wrap($msg);
-  socket_write($client,$msg,strlen($msg));
+function fnConnectacceptedSocket($socket, $client) {
+
+    GLOBAL $clients;
+    GLOBAL $client_list;
+
+    $clients[$socket]["id"] = uniqid();
+    $clients[$socket]["socket"] = $socket;
+    $clients[$socket]["handshake"] = false;
+    console("Accepted client \n\n");
+
+    $client_list[$socket] = $client;
 }
 
-function WebSocket($address,$port){
-  $master=socket_create(AF_INET, SOCK_STREAM, SOL_TCP)     or die("socket_create() failed");
-  socket_set_option($master, SOL_SOCKET, SO_REUSEADDR, 1)  or die("socket_option() failed");
-  socket_bind($master, $address, $port)                    or die("socket_bind() failed");
-  socket_listen($master,20)                                or die("socket_listen() failed");
-  echo "Server Started : ".date('Y-m-d H:i:s')."\n";
-  echo "Master socket  : ".$master."\n";
-  echo "Listening on   : ".$address." port ".$port."\n\n";
-  return $master;
+function getClientBySocket($socket) {
+    GLOBAL $client_list;
+    return $client_list[$socket];
 }
 
-function connect($socket){
-  global $sockets,$users;
-  $user = new User();
-  $user->id = uniqid();
-  $user->socket = $socket;
-  array_push($users,$user);
-  array_push($sockets,$socket);
-  console($socket." CONNECTED!");
-}
+function handshake($client, $headers, $socket) {
 
-function disconnect($socket){
-  global $sockets,$users;
-  $found=null;
-  $n=count($users);
-  for($i=0;$i<$n;$i++){
-    if($users[$i]->socket==$socket){ $found=$i; break; }
-  }
-  if(!is_null($found)){ array_splice($users,$found,1); }
-  $index = array_search($socket,$sockets);
-  socket_close($socket);
-  console($socket." DISCONNECTED!");
-  if($index>=0){ array_splice($sockets,$index,1); }
-}
-
-function dohandshake($user,$buffer){
-  console("\nRequesting handshake...");
-  console($buffer);
-  list($resource,$host,$origin,$strkey1,$strkey2,$data) = getheaders($buffer);
-  console("Handshaking...");
-
-  $pattern = '/[^\d]*/';
-  $replacement = '';
-  $numkey1 = preg_replace($pattern, $replacement, $strkey1);
-  $numkey2 = preg_replace($pattern, $replacement, $strkey2);
-
-  $pattern = '/[^ ]*/';
-  $replacement = '';
-  $spaces1 = strlen(preg_replace($pattern, $replacement, $strkey1));
-  $spaces2 = strlen(preg_replace($pattern, $replacement, $strkey2));
-
-  if ($spaces1 == 0 || $spaces2 == 0 || $numkey1 % $spaces1 != 0 || $numkey2 % $spaces2 != 0) {
-        socket_close($user->socket);
-        console('failed');
+    if (preg_match("/Sec-WebSocket-Version: (.*)\r\n/", $headers, $match))
+        $version = $match[1];
+    else {
+        console("The client doesn't support WebSocket");
         return false;
-  }
+    }
 
-  $ctx = hash_init('md5');
-  hash_update($ctx, pack("N", $numkey1/$spaces1));
-  hash_update($ctx, pack("N", $numkey2/$spaces2));
-  hash_update($ctx, $data);
-  $hash_data = hash_final($ctx,true);
+    if ($version == 13) {
+        // Extract header variables
+        if (preg_match("/GET (.*) HTTP/", $headers, $match))
+            $root = $match[1];
+        if (preg_match("/Host: (.*)\r\n/", $headers, $match))
+            $host = $match[1];
+        if (preg_match("/Origin: (.*)\r\n/", $headers, $match))
+            $origin = $match[1];
+        if (preg_match("/Sec-WebSocket-Key: (.*)\r\n/", $headers, $match))
+            $key = $match[1];
 
-  $upgrade  = "HTTP/1.1 101 WebSocket Protocol Handshake\r\n" .
-              "Upgrade: WebSocket\r\n" .
-              "Connection: Upgrade\r\n" .
-              "Sec-WebSocket-Origin: " . $origin . "\r\n" .
-              "Sec-WebSocket-Location: ws://" . $host . $resource . "\r\n" .
-              "\r\n" .
-              $hash_data;
+        $acceptKey = $key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+        $acceptKey = base64_encode(sha1($acceptKey, true));
 
-  socket_write($user->socket,$upgrade.chr(0),strlen($upgrade.chr(0)));
-  $user->handshake=true;
-  console($upgrade);
-  console("Done handshaking...");
-  return true;
+        $upgrade = "HTTP/1.1 101 Switching Protocols\r\n" .
+                "Upgrade: websocket\r\n" .
+                "Connection: Upgrade\r\n" .
+                "Sec-WebSocket-Accept: $acceptKey" .
+                "\r\n\r\n";
+
+        socket_write($client, $upgrade);
+        return true;
+    }
+    else {
+        console("WebSocket version 13 required (the client supports version {$version})");
+        return false;
+    }
 }
 
-function getheaders($req){
-  $r=$h=$o=null;
-  if(preg_match("/GET (.*) HTTP/"   ,$req,$match)){ $r=$match[1]; }
-  if(preg_match("/Host: (.*)\r\n/"  ,$req,$match)){ $h=$match[1]; }
-  if(preg_match("/Origin: (.*)\r\n/",$req,$match)){ $o=$match[1]; }
-  if(preg_match("/Sec-WebSocket-Key2: (.*)\r\n/",$req,$match)){ $key2=$match[1]; }
-  if(preg_match("/Sec-WebSocket-Key1: (.*)\r\n/",$req,$match)){ $key1=$match[1]; }
-  if(preg_match("/\r\n(.*?)\$/",$req,$match)){ $data=$match[1]; }
-  return array($r,$h,$o,$key1,$key2,$data);
+function console($text) {
+    $File = "log.txt";
+    $Handle = fopen($File, 'a');
+    fwrite($Handle, $text);
+    fclose($Handle);
 }
 
-function getuserbysocket($socket){
-  global $users;
-  $found=null;
-  foreach($users as $user){
-    if($user->socket==$socket){ $found=$user; break; }
-  }
-  return $found;
-}
-
-function     say($msg=""){ echo $msg."\n"; }
-function    wrap($msg=""){ return chr(0).$msg.chr(255); }
-function  unwrap($msg=""){ return substr($msg,1,strlen($msg)-2); }
-function console($msg=""){ global $debug; if($debug){ echo $msg."\n"; } }
-
-class User{
-  var $id;
-  var $socket;
-  var $handshake;
-}
+?>
